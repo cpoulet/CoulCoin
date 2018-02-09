5e96532eadd0875d4166f283c32be79353df39e6
Cedric Poulet <cpoulet@student.42.fr>
2018-02-09 09:41:44 +0100

    added brigade functions & envelope system

--- contracts/Coulcoin.sol
+++ contracts/Coulcoin.sol
@@ -6,23 +6,55 @@ import "./Other.sol";
 
 contract CoulCoin {
 
-	address public minter;
+	address private admin;
+    uint private    envelope;
+    address public  brigade;
 	mapping (address => uint) balances;
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Transfer(address indexed _from, address indexed _to, uint256 _amount);
 
 	// Contract creation
 	function CoulCoin() public {
-		minter = msg.sender;
+		admin = msg.sender;
 		balances[tx.origin] = 10000;
 	}
 
 	// Token creation
-	function mint(address student, uint amount) public returns(bool){
-		if (msg.sender != minter) return false;
-		balances[student] += amount;
+	function mint(address team, uint amount) public returns(bool){
+		if (msg.sender != admin) return false;
+		balances[team] += amount;
 		return true;
 	}
+
+    // Choose a new Brigade
+    function setBrigade(address team) public returns(bool){
+		if (msg.sender != admin) return false;
+        brigade = team;
+        return true;
+    }
+
+    // Reward from Brigade or Admin
+    function reward(address team, uint amount) public returns(bool) {
+		if (msg.sender != admin && msg.sender != brigade) return false;
+        if (amount > envelope) return false;
+        envelope -= amount;
+        balances[team] += amount;
+        return true;
+    }
+
+    // Fill Brigade's Envelope
+    function fillEnvelope(uint amount) public returns(bool){
+		if (msg.sender != admin) return false;
+        envelope = amount;
+        return true;
+    }
+
+    // Empty Brigade's Envelope
+    function emptyEnvelope() public returns(bool){
+		if (msg.sender != admin) return false;
+        envelope = 0;
+        return true;
+    }
 
 	// Transfert function
 	function sendCoin(address receiver, uint amount) public returns(bool) {
@@ -38,8 +70,8 @@ contract CoulCoin {
 		return Other.getData();
 	}
 
-	// Getting balance of a student
-	function getBalance(address student) public view returns(uint) {
-		return balances[student];
+	// Getting balance of a team
+	function getBalance(address team) public view returns(uint) {
+		return balances[team];
 	}
 }
